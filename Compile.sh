e103a1f396911598f8b3335997da88357802681b
Amy Finck <54650318+amyfinck@users.noreply.github.com>
2023-06-07 01:46:26 -0700

    Added simple compiling shell script to run on ugls

---
+++ Compile.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+mkdir -p /tmp/tmp_dir/
+cp ./main.c /tmp/tmp_dir/
+
+process_id=$!
+
+/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/tmp_dir/main.c -o
+wait $process_id
