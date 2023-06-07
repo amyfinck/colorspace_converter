425bcaa82473f09395dd42c7dd866fec9a4dab84
Amy Finck <54650318+amyfinck@users.noreply.github.com>
2023-06-07 02:09:56 -0700

    update script + readme

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 - `ssh ugls1.ece.uvic.ca`
 - type in uvic credentials
 - clone this repo
-- `$ chmod +x ./compile.sh`
-- `$ ./compile.sh`
+- `chmod +x ./compile.sh`
+- `./compile.sh`
 - check output
--- compile.sh
+++ compile.sh
@@ -5,5 +5,5 @@ cp ./main.c /tmp/tmp_dir/
 
 process_id=$!
 
-/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/tmp_dir/main.c -o
+/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/tmp_dir/main.c
 wait $process_id
