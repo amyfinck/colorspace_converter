4dbf7c8ee2236dccf1f8fa1df1504d2b982dbdcb
Amy Finck <54650318+amyfinck@users.noreply.github.com>
2023-06-12 10:44:57 -0700

    Add shell script to compile

--- compile.sh
+++ compile.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
+process_id=$!
+
+echo Copying files to /tmp/tmp_dir/
 
 mkdir -p /tmp/tmp_dir/
 cp ./main.c /tmp/tmp_dir/
+# add more files as they are added to our project
 
-process_id=$!
-
-/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/tmp_dir/main.c
-/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/tmp_dir/main.c -o main.exe
+echo Compiling main.c
+/opt/arm/4.3.2/bin/arm-linux-gcc -static -mfloat-abi=softfp -mfpu=neon -O3 /tmp/tmp_dir/main.c -o main.exe
 
 wait $process_id
+
+echo Compile complete, output is main.exe
