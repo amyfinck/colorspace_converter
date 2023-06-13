083e355fa4c775a00ca4683b18818f9484b7b753
amyfinck <amyfinck@outlook.com>
2023-06-12 22:55:08 -0700

    new try w arm

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ uint32x4_t aa, bb, ss;
 
 int main( void )
 {
+    printf("hello\n");
     const uint32_t init_aa[4] = {1, 2, 3, 4};
     const uint32_t init_bb[4] = {5, 6, 7, 8};
     aa = vld1q_u32((uint32_t*)&init_aa);
@@ -15,7 +16,7 @@ int main( void )
     ss = vaddq_u32( aa, bb);
     int32_t* res_ptr = (int32_t*)&ss;
 
-    printf("Result: %d, %d, %d, %d\n", res_ptr[0], res_ptr[1], res_ptr[2], res_ptr[3]);
+    printf("Result: %u, %u, %u, %u\n", res_ptr[0], res_ptr[1], res_ptr[2], res_ptr[3]);
 
     return 0;
 }
