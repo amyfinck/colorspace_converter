feaa57c6cd7ab21752f76ab6b95329d370ef64b2
Amy Finck <54650318+amyfinck@users.noreply.github.com>
2023-06-23 13:25:14 -0700

    add test image and update cc

--- colorspace_converter.c
+++ colorspace_converter.c
@@ -11,9 +11,9 @@
 // struct for BMP header info
 typedef struct BMP_header_t
 {
-    // TODO fill with information contained in a BMP header
     uint32_t height;
     uint32_t width;
+    uint32_t offset;
 }BMP_header_t;
 
 typedef struct RGB_pixel_t
@@ -23,6 +23,13 @@ typedef struct RGB_pixel_t
     uint8_t B;
 }RGB_pixel_t;
 
+typedef struct RGB_image_t
+{
+    RGB_pixel_t *pixels;
+    BMP_header_t header;
+    uint32_t pixel_count;
+}RGB_image_t;
+
 typedef struct YCC_pixel_t
 {
     uint8_t Y; // luma
@@ -30,9 +37,15 @@ typedef struct YCC_pixel_t
     uint8_t Cr; // red-difference
 }YCC_pixel_t;
 
+typedef struct YCC_image_t
+{
+    YCC_pixel_t *pixels;
+    BMP_header_t header;
+}YCC_image_t;
 
 BMP_header_t* getHeaderBMP(FILE* fp, BMP_header_t* header)
 {
+    // TODO implement here instead of in main
     return header;
 }
 
@@ -78,8 +91,42 @@ int main( int argc, char* argv[] )
         exit(1);
     }
 
+    memcpy(&BMPHeader->offset, (p + 10), 4);
     memcpy(&BMPHeader->width, (p + 18), 4);
     memcpy(&BMPHeader->height, (p + 22), 4);
+
+    RGB_image_t *inputImage = malloc(sizeof(RGB_image_t));
+    inputImage->pixel_count = 0;
+    if(inputImage == NULL)
+    {
+        printf("error - malloc failed\n");
+        close(file_descriptor);
+        munmap(p, statbuf.st_size);
+        exit(1);
+    }
+    inputImage->header = *BMPHeader;
+    inputImage->pixels = malloc(sizeof(RGB_pixel_t) * BMPHeader->height * BMPHeader->width);
+    if(inputImage->pixels == NULL)
+    {
+        printf("error - malloc failed\n");
+        close(file_descriptor);
+        munmap(p, statbuf.st_size);
+        exit(1);
+    }
+
+    for(int i = 0; i < inputImage->header.width * inputImage->header.height; i++)
+    {
+        // TODO RGB vs RGBA
+        memcpy(&inputImage->pixels->R, (p + BMPHeader->offset + (i * 3)), 1);
+        memcpy(&inputImage->pixels->G, (p + BMPHeader->offset + (i * 3) + 1), 1);
+        memcpy(&inputImage->pixels->B, (p + BMPHeader->offset + (i * 3) + 2), 1);
+        inputImage->pixel_count++;
+    }
+
+    for(int i = 0; i < inputImage->pixel_count; i++)
+    {
+        printf("R: %d G: %d B:%d\n", (inputImage->pixels + i)->R, (inputImage->pixels + i)->G, (inputImage->pixels + i)->B);
+    }
 
     printf("I received a picture with a height of %d and a width of %d\n", BMPHeader->height, BMPHeader->width);
 
