4d6958e01096b6d1d9d1d839adf4a40c492af48c
Amy Finck <54650318+amyfinck@users.noreply.github.com>
2023-07-09 13:14:56 -0700

    Cleaned up main and add parrot example file

--- colorspace_converter.c
+++ colorspace_converter.c
@@ -28,8 +28,8 @@ typedef struct RGB_image_t
 typedef struct YCC_pixel_t
 {
     uint8_t Y; // luma
-    uint8_t Cb; // blue-difference
-    uint8_t Cr; // red-difference
+    int8_t Cb; // blue-difference
+    int8_t Cr; // red-difference
 }YCC_pixel_t;
 
 typedef struct YCC_image_t
@@ -62,10 +62,37 @@ void getChroma()
 {
     for(uint32_t i = 0; i < inputImage->pixel_count; i++)
     {
-        outputImage->pixels[i].Cb = (inputImage->pixels[i].B - outputImage->pixels[i].Y);
-        outputImage->pixels[i].Cr = (inputImage->pixels[i].R - outputImage->pixels[i].Y);
+        // Cb and Cr are signed from -128 to 127
+        int32_t Cb = inputImage->pixels[i].B - outputImage->pixels[i].Y;
+        int32_t Cr = inputImage->pixels[i].R - outputImage->pixels[i].Y;
+        if(Cb > 127) Cb = 127;
+        if(Cb < -128) Cb = -128;
+        if(Cr > 127) Cr = 127;
+        if(Cr < -128) Cr = -128;
+
+        outputImage->pixels[i].Cb = (int8_t)Cb;
+        outputImage->pixels[i].Cr = (int8_t)Cr;
     }
 }
+
+void downsampleChroma()
+{
+    uint32_t sections = inputImage->pixel_count / 4;
+    for(uint32_t i = 0; i < inputImage->pixel_count; i++)
+    {
+        // Cb and Cr are signed from -128 to 127
+        int32_t Cb = inputImage->pixels[i].B - outputImage->pixels[i].Y;
+        int32_t Cr = inputImage->pixels[i].R - outputImage->pixels[i].Y;
+        if(Cb > 127) Cb = 127;
+        if(Cb < -128) Cb = -128;
+        if(Cr > 127) Cr = 127;
+        if(Cr < -128) Cr = -128;
+
+        outputImage->pixels[i].Cb = (int8_t)Cb;
+        outputImage->pixels[i].Cr = (int8_t)Cr;
+    }
+}
+
 
 void print_pixel( uint32_t x, uint32_t y)
 {
@@ -77,7 +104,6 @@ int main( int argc, char* argv[] )
 {
     // TODO clear some stuff out of main
     // TODO check bitmap format
-
     if(argc != 2)
     {
         printf("Not a valid command. Usage: ./colorspace_converter <img_name>.bmp\n");
@@ -90,63 +116,48 @@ int main( int argc, char* argv[] )
     if (in_fd == -1)
     {
         close(in_fd);
-        printf("input File error\n");
+        printf("Could not open %s\n", argv[1]);
         exit(1);
     }
+    struct stat sb;
+    fstat(in_fd, &sb);
 
-    // fstat gets the file status, and puts all the relevant information in sb
-    struct stat statbuf;
-    fstat(in_fd, &statbuf);
-
-    // mmap the file to pointer p
-    char * p = mmap(NULL, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, in_fd, 0);
-    if (fstat(in_fd, &statbuf) == -1 | p == MAP_FAILED)
+    // map contents of file to memory pointer p
+    char * p = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, in_fd, 0);
+    if (fstat(in_fd, &sb) == -1 | p == MAP_FAILED)
     {
         printf("Error: failed to map memory\n");
         close(in_fd);
         exit(1);
     }
-    size_t file_size = statbuf.st_size;
+    size_t file_size = sb.st_size;
 
-    printf("file size: %ld\n", file_size);
-
-    char buffer[file_size];
-    ssize_t bytesRead, bytesWritten;
-
+    // create input image struct and fill in info from header
     inputImage = malloc(sizeof(RGB_image_t));
-
+    if(inputImage == NULL)
+    {
+        printf("error - malloc failed\n"); close(in_fd); munmap(p, sb.st_size); exit(1);
+    }
     memcpy(&inputImage->offset, (p + 10), 4);
     memcpy(&inputImage->width, (p + 18), 4);
     memcpy(&inputImage->height, (p + 22), 4);
-
     inputImage->pixel_count = 0;
-    if(inputImage == NULL)
-    {
-        printf("error - malloc failed\n");
-        close(in_fd);
-        munmap(p, statbuf.st_size);
-        exit(1);
-    }
-
     inputImage->pixels = malloc(sizeof(RGB_pixel_t) * inputImage->height * inputImage->width);
     if(inputImage->pixels == NULL)
     {
-        printf("error - malloc failed\n");
-        close(in_fd);
-        munmap(p, statbuf.st_size);
-        exit(1);
+        printf("error - malloc failed\n"); close(in_fd); munmap(p, sb.st_size); exit(1);
     }
 
-    // TODO check header is valid
-    uint32_t bytesPerRow = inputImage->width * 3; // there are 3 bytes per pixel
+    // calculate padding
+    uint32_t bytesPerRow = inputImage->width * 3; // 3 bytes per pixel
     uint32_t padding = 0;
     if (bytesPerRow % 4 != 0)
     {
         padding = 4 - (bytesPerRow % 4);
+        bytesPerRow += padding;
     }
-    bytesPerRow += padding;
-    inputImage->pixel_count = 0;
 
+    // read in pixels
     uint32_t pixel_offset = 0;
     uint32_t pixel_index = 0;
     for(uint32_t y = 0; y < inputImage->height; y++)
@@ -164,12 +175,12 @@ int main( int argc, char* argv[] )
             inputImage->pixel_count++;
         }
     }
-    munmap(p, statbuf.st_size);
 
+    // create a file for all 4 output types
     int luma_fd;
     int cb_fd;
     int cr_fd;
-
+    int ycc_fd;
     chdir("luma");
     luma_fd = open(argv[1], O_RDWR | O_CREAT, 0666);
     if (luma_fd == -1)
@@ -198,6 +209,9 @@ int main( int argc, char* argv[] )
     }
     chdir("..");
 
+    // copy input file to output locations
+    char buffer[file_size];
+    ssize_t bytesRead, bytesWritten;
     while ((bytesRead = read(in_fd, buffer, file_size)) > 0) {
         bytesWritten = write(luma_fd, buffer, bytesRead);
         write(cb_fd, buffer, bytesRead);
@@ -205,80 +219,84 @@ int main( int argc, char* argv[] )
         if (bytesWritten == -1) {
             printf("Error writing to output file.\n");
             close(in_fd);
-            close(in_fd);
             return 1;
         }
     }
 
+    // create output image struct and fill in info
     outputImage = malloc(sizeof(YCC_image_t));
-    outputImage->pixel_count = 0;
     if(outputImage == NULL)
     {
-        printf("error - malloc failed\n");
-        close(in_fd);
-        munmap(p, statbuf.st_size);
-        exit(1);
+        printf("Error - Output malloc failed\n"); close(in_fd); close(luma_fd); close(cb_fd); close(cr_fd); munmap(p, sb.st_size); exit(1);
     }
-
+    outputImage->pixel_count = 0;
     outputImage->offset = inputImage->offset;
     outputImage->width = inputImage->width;
     outputImage->height = inputImage->height;
-
     outputImage->pixels = malloc(sizeof(YCC_pixel_t) * outputImage->height * outputImage->width);
     if(outputImage->pixels == NULL)
     {
-        printf("error - malloc failed\n");
-        close(in_fd);
-        close(luma_fd);
-        munmap(p, statbuf.st_size);
-        exit(1);
+        printf("error - Output pix malloc failed\n"); close(in_fd); close(luma_fd); close(cb_fd); close(cr_fd); munmap(p, sb.st_size); exit(1);
     }
 
+    // Calculate YCC values for OutputImage
     getLuma();
     getChroma();
+    downsampleChroma();
 
-    char * p_l = mmap(NULL, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, luma_fd, 0);
-    char * p_cb = mmap(NULL, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, cb_fd, 0);
-    char * p_cr = mmap(NULL, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, cr_fd, 0);
+    // create mappings for output files
+    char * p_l = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, luma_fd, 0);
+    char * p_cb = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, cb_fd, 0);
+    char * p_cr = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, cr_fd, 0);
 
+    // write YCC values to output files
     pixel_offset = 0;
     pixel_index = 0;
-
     for(uint32_t y = 0; y < outputImage->height; y++)
     {
         for (uint32_t x = 0; x < outputImage->width; x++)
         {
+            // Pixels are stored in BGR order
             pixel_offset = (y * bytesPerRow) + (x * 3);
 
-            // Pixels are stored in BGR order
+            // Write luma output file (grayscale)
             memcpy((p_l + outputImage->offset + pixel_offset), &outputImage->pixels[pixel_index].Y, sizeof(uint8_t));
             memcpy((p_l + outputImage->offset + pixel_offset + 1), &outputImage->pixels[pixel_index].Y, sizeof(uint8_t));
             memcpy((p_l + outputImage->offset + pixel_offset + 2), &outputImage->pixels[pixel_index].Y, sizeof(uint8_t));
 
+            // Convert Cb and Cr to unsigned for use in RGB representation
             uint8_t baseColor = 128;
             uint8_t Cb = outputImage->pixels[pixel_index].Cb + 128;
             uint8_t Cr = outputImage->pixels[pixel_index].Cr + 128;
 
+            // Write blue difference output file
             memcpy((p_cb + outputImage->offset + pixel_offset), &Cb, sizeof(uint8_t));
             memcpy((p_cb + outputImage->offset + pixel_offset + 1), &baseColor, sizeof(uint8_t));
             memcpy((p_cb + outputImage->offset + pixel_offset + 2), &baseColor, sizeof(uint8_t));
 
+            // Write red difference output file
             memcpy((p_cr + outputImage->offset + pixel_offset), &baseColor, sizeof(uint8_t));
             memcpy((p_cr + outputImage->offset + pixel_offset + 1), &baseColor, sizeof(uint8_t));
             memcpy((p_cr + outputImage->offset + pixel_offset + 2), &Cr, sizeof(uint8_t));
 
             pixel_index++;
-            inputImage->pixel_count++;
+            outputImage->pixel_count++;
         }
     }
 
-    munmap(p_l, statbuf.st_size);
-    msync(p_l, statbuf.st_size, MS_SYNC);
+    // unmap
+    munmap(p, sb.st_size);
+    munmap(p_l, sb.st_size);
+    munmap(p_cb, sb.st_size);
+    munmap(p_cr, sb.st_size);
 
+    // close all files
     close(in_fd);
     close(luma_fd);
     close(cb_fd);
     close(cr_fd);
+
+    printf("Luma component: /luma/%s\nCb component: /cb/%s\nCr component: /cr/%s\n", argv[1], argv[1], argv[1]);
 
     return 0;
 }
