6ad3a0bc2116429cda4da81fdbb5a2178d865f83
Amy Finck <54650318+amyfinck@users.noreply.github.com>
2023-07-16 11:20:10 -0700

    update shell script

--- compile.sh
+++ compile.sh
@@ -4,12 +4,13 @@ process_id=$!
 echo Copying files to /tmp/tmp_dir/
 
 mkdir -p /tmp/tmp_dir/
-cp ./main.c /tmp/tmp_dir/
+cp ./unoptimized/colorspace_converter.c /tmp/tmp_dir/
+cp ./unoptimized/colorspace_converter.h /tmp/tmp_dir/
 # add more files as they are added to our project
 
-echo Compiling main.c
-/opt/arm/4.3.2/bin/arm-linux-gcc -static -mfloat-abi=softfp -mfpu=neon -O3 /tmp/tmp_dir/main.c -o main.exe
+echo Compiling unoptimized colorspace converter
+/opt/arm/4.3.2/bin/arm-linux-gcc -static -mfloat-abi=softfp -mfpu=neon -O3 /tmp/tmp_dir/colorspace_converter.c -o cc.exe
 
 wait $process_id
 
-echo Compile complete, output is main.exe
+echo Compile complete, output is cc.exe
