3c5212a79e63e2b2c097e0aa0eda86f91da75db1
Adil Zafar <adil.khan@primeeng.ca>
2023-07-19 00:23:20 -0700

    Move utility functions to utilities file

---
+++ unoptimized/utilities.h
@@ -0,0 +1,36 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include "constants.h"
+
+void allocate_rgb_pixels_memory(RGB_image_t *rgb_img)
+{
+    rgb_img->pixel_count = 0;
+    rgb_img->pixels = (RGB_pixel_t *)malloc(sizeof(RGB_pixel_t) * rgb_img->height * rgb_img->width);
+    exit_on_error(rgb_img->pixels == NULL, "Error: Malloc for RGB pixels failed");
+}
+
+void allocate_ycc_pixels_memory(YCC_image_t *ycc_img)
+{
+    ycc_img->pixel_count = 0;
+    ycc_img->pixels = (YCC_pixel_t *)malloc(sizeof(YCC_pixel_t) * ycc_img->height * ycc_img->width);
+    exit_on_error(ycc_img->pixels == NULL, "Error: Malloc for YCC pixels failed");
+}
+
+void copy_rgb_to_yyc(RGB_image_t *rgb_in_img, YCC_image_t *ycc_out_img)
+{
+    ycc_out_img->offset = rgb_in_img->offset;
+    ycc_out_img->width = rgb_in_img->width;
+    ycc_out_img->height = rgb_in_img->height;
+    allocate_ycc_pixels_memory(ycc_out_img);
+}
+
+void copy_rgb_to_rgb(RGB_image_t *rgb_in_img, RGB_image_t *rgb_out_img)
+{
+    rgb_out_img->offset = rgb_in_img->offset;
+    rgb_out_img->width = rgb_in_img->width;
+    rgb_out_img->height = rgb_in_img->height;
+    allocate_rgb_pixels_memory(rgb_out_img);
+}
+
+#endif
