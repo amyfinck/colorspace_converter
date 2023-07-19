ed39f00113ea6642093118c77eb84eb8d35ee4b0
Adil Zafar <adil.khan@primeeng.ca>
2023-07-19 00:49:18 -0700

    Fix variable name

--- unoptimized/colorspace_converter2.c
+++ unoptimized/colorspace_converter2.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "converters.h"
 #include "bmp_parser.h"
+#include "bmp_handler.h"
 #include "utilities.h"
 
 int main(int argc, char *argv[])
@@ -9,7 +10,7 @@ int main(int argc, char *argv[])
     // Declarations
     RGB_image_t *rgb_in_img;
     RGB_image_t *rgb_out_img;
-    YCC_image_t *yyc_out_img;
+    YCC_image_t *ycc_out_img;
 
     FILE *in_file_rgb;
     FILE *out_file_cb;
@@ -24,17 +25,31 @@ int main(int argc, char *argv[])
     out_file_luma = fopen(argv[1], "w+");
     out_file_rgb = fopen(argv[1], "w+");
 
-    //----------------------------------
+    // Run processes
     set_img_header_from_file(rgb_in_img, in_file_rgb);
     allocate_rgb_pixels_memory(rgb_in_img);
     set_img_pixels_from_file(rgb_in_img, in_file_rgb);
 
+    // Copy info from input file to output files
+    copy_file(in_file_rgb, out_file_cb);
+    copy_file(in_file_rgb, out_file_cr);
+    copy_file(in_file_rgb, out_file_luma);
+    copy_file(in_file_rgb, out_file_rgb);
+
+    // Copy info from input image to output imagess
+    copy_rgb_to_yyc(rgb_in_img, ycc_out_img);
+    copy_rgb_to_rgb(rgb_in_img, rgb_out_img);
+
+    // Compute and allocate conversions
+    compute_ycc_from_rgb(rgb_in_img, ycc_out_img);
+    compute_rgb_from_ycc(ycc_out_img, rgb_out_img);
+
     // Free memory
     free(rgb_in_img->pixels);
-    free(yyc_out_img->pixels);
+    free(ycc_out_img->pixels);
     free(rgb_in_img);
     free(rgb_out_img);
-    free(yyc_out_img);
+    free(ycc_out_img);
 
     // Close files
     fclose(in_file_rgb);
