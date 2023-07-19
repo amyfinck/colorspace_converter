d441948bbd74ed26e94044986e80a949f13b7a38
Adil Zafar <adil.khan@primeeng.ca>
2023-07-18 21:18:15 -0700

    add function to downsample ycc chroma

--- unoptimized/converters.h
+++ unoptimized/converters.h
@@ -54,11 +54,42 @@ void compute_rgb_from_ycc(YCC_image_t *yyc_out_img, RGB_image_t *rgb_in_img)
     }
 }
 
-void downsample_ycc_chroma()
+void downsample_ycc_chroma(RGB_image_t *rgb_in_img, YCC_image_t *yyc_out_img)
 {
+    uint32_t row, column, i;
+    uint8_t sample_size = 4;
+    for (row = 0; row < rgb_in_img->height; row += (sample_size / 2))
+    {
+        for (column = 0; column < rgb_in_img->width; column += (sample_size / 2))
+        {
+            uint32_t index = row * rgb_in_img->width + column;
+            uint32_t pixels[] = {
+                index,
+                index + 1,
+                index + rgb_in_img->width,
+                index + rgb_in_img->width + 1
+            };
 
+            int32_t cb_total = 0;
+            int32_t cr_total = 0;
+
+            for (i = 0; i < sample_size; ++i)
+            {
+                cb_total += yyc_out_img->pixels[pixels[i]].Cb;
+                cr_total += yyc_out_img->pixels[pixels[i]].Cr;
+            }
+
+            int8_t cb_avg = (int8_t)(cb_total / sample_size);
+            int8_t cr_avg = (int8_t)(cr_total / sample_size);
+
+            for (i = 0; i < sample_size; ++i)
+            {
+                yyc_out_img->pixels[pixels[i]].Cb = cb_avg;
+                yyc_out_img->pixels[pixels[i]].Cr = cr_avg;
+            }
+        }
+    }
 }
-
 
 void rgb_to_ycc();
 void ycc_to_ycc();
