5ff7b4f24d2cc0a07d00a9b85ce9ce034c3760e4
Adil Zafar <adil.khan@primeeng.ca>
2023-07-19 19:16:21 -0700

    oo

--- unoptimized/bmp_parser.h
+++ unoptimized/bmp_parser.h
@@ -15,7 +15,7 @@ uint32_t get_file_size(FILE *in_file)
 
 uint32_t *get_file_offset(FILE *in_file)
 {
-    uint32_t *buffer;
+    uint32_t *buffer = (uint32_t)malloc(sizeof(uint32_t));
     exit_on_error(fseek(in_file, 10, SEEK_SET) != 0, "Seeking offset position failed");
     exit_on_error(fread(buffer, 4, 1, in_file) == 0, "Reading file offset failed");
     return buffer;
