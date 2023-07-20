befc1aa6b38a7f597e6da7d71732e765f4d61cfc
Adil Zafar <adil.khan@primeeng.ca>
2023-07-19 18:56:10 -0700

    oo

--- unoptimized/colorspace_converter2.c
+++ unoptimized/colorspace_converter2.c
@@ -60,12 +60,12 @@ int main(int argc, char *argv[])
     // ycc_pixels_file_handler(ycc_out_img, out_file_cr, write_cr);
     // rgb_pixels_file_handler(rgb_out_img, out_file_rgb, write_rgb);
 
-    // Free memory
-    free(rgb_in_img->pixels);
-    free(ycc_out_img->pixels);
-    free(rgb_in_img);
-    free(rgb_out_img);
-    free(ycc_out_img);
+    // // Free memory
+    // free(rgb_in_img->pixels);
+    // free(ycc_out_img->pixels);
+    // free(rgb_in_img);
+    // free(rgb_out_img);
+    // free(ycc_out_img);
 
     // Close files
     fclose(in_file_rgb);
