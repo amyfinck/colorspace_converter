057a009471eaaff23abdd70e7cdc647d8ff63ee0
Adil Zafar <adil.khan@primeeng.ca>
2023-07-19 19:31:22 -0700

    oo

--- unoptimized/utilities.h
+++ unoptimized/utilities.h
@@ -6,27 +6,27 @@
 void allocate_rgb_memory(RGB_image_t *rgb_img)
 {
     rgb_img = (RGB_image_t *)malloc(sizeof(RGB_image_t));
-    exit_on_error(rgb_img == NULL, "Cannot allocate memory to rgb image");
+    exit_on_error(rgb_img == NULL, "Malloc for rgb image failed");
 }
 
 void allocate_rgb_pixels_memory(RGB_image_t *rgb_img)
 {
     rgb_img->pixel_count = 0;
     rgb_img->pixels = (RGB_pixel_t *)malloc(sizeof(RGB_pixel_t) * rgb_img->height * rgb_img->width);
-    exit_on_error(rgb_img->pixels == NULL, "Error: Malloc for RGB pixels failed");
+    exit_on_error(rgb_img->pixels == NULL, "Malloc for RGB pixels failed");
 }
 
 void allocate_ycc_pixels_memory(YCC_image_t *ycc_img)
 {
     ycc_img->pixel_count = 0;
     ycc_img->pixels = (YCC_pixel_t *)malloc(sizeof(YCC_pixel_t) * ycc_img->height * ycc_img->width);
-    exit_on_error(ycc_img->pixels == NULL, "Error: Malloc for YCC pixels failed");
+    exit_on_error(ycc_img->pixels == NULL, "Malloc for YCC pixels failed");
 }
 
 void copy_rgb_to_yyc(RGB_image_t *rgb_in_img, YCC_image_t *ycc_out_img)
 {
     ycc_out_img = malloc(sizeof(YCC_image_t));
-    exit_on_error(ycc_out_img == NULL, "Error: Malloc for YCC image failed");
+    exit_on_error(ycc_out_img == NULL, "Malloc for YCC image failed");
     ycc_out_img->offset = rgb_in_img->offset;
     ycc_out_img->width = rgb_in_img->width;
     ycc_out_img->height = rgb_in_img->height;
@@ -36,7 +36,7 @@ void copy_rgb_to_yyc(RGB_image_t *rgb_in_img, YCC_image_t *ycc_out_img)
 void copy_rgb_to_rgb(RGB_image_t *rgb_in_img, RGB_image_t *rgb_out_img)
 {
     rgb_out_img = malloc(sizeof(RGB_image_t));
-    exit_on_error(rgb_out_img == NULL, "Error: Malloc for RGB image failed");
+    exit_on_error(rgb_out_img == NULL, "Malloc for RGB image failed");
     rgb_out_img->offset = rgb_in_img->offset;
     rgb_out_img->width = rgb_in_img->width;
     rgb_out_img->height = rgb_in_img->height;
