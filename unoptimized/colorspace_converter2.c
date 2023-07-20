43d26d126c812d24d662c6ecc46c6bc2302af72a
Adil Zafar <adil.khan@primeeng.ca>
2023-07-19 19:58:33 -0700

    oo

--- unoptimized/colorspace_converter2.c
+++ unoptimized/colorspace_converter2.c
@@ -39,7 +39,9 @@ int main(int argc, char *argv[])
 
     // Read rgb input file
     set_img_header_from_file(rgb_in_img, in_file_rgb);
-    rgb_pixels_file_handler(rgb_in_img, in_file_rgb, read_rgb);
+    printf("%d\n", rgb_in_img->height);
+
+    // rgb_pixels_file_handler(rgb_in_img, in_file_rgb, read_rgb);
 
     // allocate_rgb_pixels_memory(rgb_in_img);
 
