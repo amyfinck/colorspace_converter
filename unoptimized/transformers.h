bbfcf19a9aeed57947cda9741ef9b80a24953a87
Adil Zafar <adil.khan@primeeng.ca>
2023-07-25 13:51:00 -0700

    oo

--- unoptimized/transformers.h
+++ unoptimized/transformers.h
@@ -3,11 +3,6 @@
 
 #include <stdint.h>
 
-#define K 8
-#define Q 8 // Number of fractional bits
-#define FP_SHIFT(x) ((x) << Q)
-#define FP_ROUND(x) (((x) + (1 << (Q - 1))) >> Q)
-
 uint8_t compute_ycc_y(uint8_t R, uint8_t G, uint8_t B)
 {
     uint32_t temp = (16 << K) + (66 * R) + (129 * G) + (25 * B);
@@ -31,19 +26,30 @@ uint8_t compute_ycc_cr(uint8_t R, uint8_t G, uint8_t B)
 
 uint8_t compute_rgb_r(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int16_t red = FP_ROUND(FP_SHIFT(Y) + FP_SHIFT(1.402) * (Cr - 128));
+    int16_t y = (int16_t)Y[i] - 16;
+    int16_t cr = (int16_t)Cr[i] - 128;
+
+    int16_t red = (298 * y + 409 * cr + 128) >> 8;
     return (uint8_t)(red < 0 ? 0 : (red > 255 ? 255 : red));
 }
 
 uint8_t compute_rgb_g(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int16_t green = FP_ROUND(Y - 0.344136 * (Cb - 128) - 0.714136 * (Cr - 128));
+    int16_t y = (int16_t)Y[i] - 16;
+    int16_t cb = (int16_t)Cb[i] - 128;
+    int16_t cr = (int16_t)Cr[i] - 128;
+
+    int16_t green = (298 * y - 100 * cb - 208 * cr + 128) >> 8;
     return (uint8_t)(green < 0 ? 0 : (green > 255 ? 255 : green));
 }
 
 uint8_t compute_rgb_b(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int16_t blue = FP_ROUND(Y + 1.772 * (Cb - 128));
+    int16_t y = (int16_t)Y[i] - 16;
+    int16_t cb = (int16_t)Cb[i] - 128;
+    int16_t cr = (int16_t)Cr[i] - 128;
+
+    int16_t blue = (298 * y + 516 * cb + 128) >> 8;
     return (uint8_t)(blue < 0 ? 0 : (blue > 255 ? 255 : blue));
 }
 
