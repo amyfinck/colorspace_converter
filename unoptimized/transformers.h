066a735a99577032e4727e26b9a3f6b98b85d39e
Adil Zafar <adil.khan@primeeng.ca>
2023-07-25 14:58:18 -0700

    oo

--- unoptimized/transformers.h
+++ unoptimized/transformers.h
@@ -7,52 +7,38 @@
 
 uint8_t compute_ycc_y(uint8_t R, uint8_t G, uint8_t B)
 {
-    uint32_t temp = (16 << K) + (66 * R) + (129 * G) + (25 * B);
+    int32_t temp = (16 << K) + (66 * R) + (129 * G) + (25 * B);
     return (uint8_t)((temp + (1 << (K - 1))) >> K);
 }
 
 uint8_t compute_ycc_cb(uint8_t R, uint8_t G, uint8_t B)
 {
-    uint32_t temp = (128 << K) - (38 * R) - (74 * G) + (112 * B);
-    uint8_t cb = (uint8_t)((temp + (1 << (K - 1))) >> K);
-    if (cb < 0)
-        return 0;
-    return cb > 255 ? 255 : cb;
+    int32_t temp = (128 << K) - (38 * R) - (74 * G) + (112 * B);
+    int32_t cb = (int32_t)((temp + (1 << (K - 1))) >> K);
+    return (uint8_t)(cb < 0 ? 0 : (cb > 255 ? 255 : cb));
 }
 
 uint8_t compute_ycc_cr(uint8_t R, uint8_t G, uint8_t B)
 {
-    uint32_t temp = (128 << K) + (112 * R) - (94 * G) - (18 * B);
+    int32_t temp = (128 << K) + (112 * R) - (94 * G) - (18 * B);
     return (uint8_t)((temp + (1 << (K - 1))) >> K);
 }
 
-uint8_t compute_rgb_r(uint8_t Y, uint8_t Cb, uint8_t Cr)
+uint8_t compute_rgb_r(int16_t Y, int16_t Cb, int16_t Cr)
 {
-    int16_t y = (int16_t)Y - 16;
-    int16_t cb = (int16_t)Cb - 128;
-    int16_t cr = (int16_t)Cr - 128;
-
-    int16_t red = (298 * y + 409 * cr + 128) >> 8;
+    int16_t red = (298 * Y + 409 * Cr + 128) >> K;
     return (uint8_t)(red < 0 ? 0 : (red > 255 ? 255 : red));
 }
 
-uint8_t compute_rgb_g(uint8_t Y, uint8_t Cb, uint8_t Cr)
+uint8_t compute_rgb_g(int16_t Y, int16_t Cb, int16_t Cr)
 {
-    int16_t y = (int16_t)Y - 16;
-    int16_t cb = (int16_t)Cb - 128;
-    int16_t cr = (int16_t)Cr - 128;
-
-    int16_t green = (298 * y - 100 * cb - 208 * cr + 128) >> 8;
+    int16_t green = (298 * Y - 100 * Cb - 208 * Cr + 128) >> K;
     return (uint8_t)(green < 0 ? 0 : (green > 255 ? 255 : green));
 }
 
-uint8_t compute_rgb_b(uint8_t Y, uint8_t Cb, uint8_t Cr)
+uint8_t compute_rgb_b(int16_t Y, int16_t Cb, int16_t Cr)
 {
-    int16_t y = (int16_t)Y - 16;
-    int16_t cb = (int16_t)Cb - 128;
-    int16_t cr = (int16_t)Cr - 128;
-
-    int16_t blue = (298 * y + 516 * cb + 128) >> 8;
+    int16_t blue = (298 * Y + 516 * Cb + 128) >> K;
     return (uint8_t)(blue < 0 ? 0 : (blue > 255 ? 255 : blue));
 }
 
