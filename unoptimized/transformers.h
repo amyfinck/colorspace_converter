f8c14270f03cceec51c59ecf57c963ee003242a7
Adil Zafar <adil.khan@primeeng.ca>
2023-07-25 13:39:14 -0700

    oo

--- unoptimized/transformers.h
+++ unoptimized/transformers.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+#define K8
 #define Q 8 // Number of fractional bits
 #define FP_SHIFT(x) ((x) << Q)
 #define FP_ROUND(x) (((x) + (1 << (Q - 1))) >> Q)
@@ -30,19 +31,19 @@ uint8_t compute_ycc_cr(uint8_t R, uint8_t G, uint8_t B)
 
 uint8_t compute_rgb_r(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int64_t red = FP_ROUND(Y + 1.402 * (Cr - 128));
+    int16_t red = FP_ROUND(Y + 1.402 * (Cr - 128));
     return (uint8_t)(red < 0 ? 0 : (red > 255 ? 255 : red));
 }
 
 uint8_t compute_rgb_g(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int64_t green = FP_ROUND(Y - 0.344136 * (Cb - 128) - 0.714136 * (Cr - 128));
+    int16_t green = FP_ROUND(Y - 0.344136 * (Cb - 128) - 0.714136 * (Cr - 128));
     return (uint8_t)(green < 0 ? 0 : (green > 255 ? 255 : green));
 }
 
 uint8_t compute_rgb_b(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int64_t blue = FP_ROUND(Y + 1.772 * (Cb - 128));
+    int16_t blue = FP_ROUND(Y + 1.772 * (Cb - 128));
     return (uint8_t)(blue < 0 ? 0 : (blue > 255 ? 255 : blue));
 }
 
