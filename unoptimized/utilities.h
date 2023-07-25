1ca2cc5adcbae4e5c18f674e820611780f896a7f
Adil <adilzafar66@gmail.com>
2023-07-24 22:00:01 -0700

    oo

--- unoptimized/utilities.h
+++ unoptimized/utilities.h
@@ -15,14 +15,14 @@ void allocate_ycc_memory(YCC_image_t **ycc_img)
     exit_on_error(*ycc_img == NULL, "Malloc for yyc image failed");
 }
 
-void allocate_rgb_pixels_memory(RGB_image_t **rgb_img)
+void allocate_rgb_pixels_memory(RGB_image_t *rgb_img)
 {
-    *rgb_img->pixel_count = 0;
-    *rgb_img->pixels = (RGB_pixel_t *)malloc(sizeof(RGB_pixel_t) * (*rgb_img->height) * (*rgb_img->width));
-    exit_on_error(*rgb_img->pixels == NULL, "Malloc for RGB pixels failed");
+    rgb_img->pixel_count = 0;
+    rgb_img->pixels = (RGB_pixel_t *)malloc(sizeof(RGB_pixel_t) * rgb_img->height * rgb_img->width);
+    exit_on_error(rgb_img->pixels == NULL, "Malloc for RGB pixels failed");
 }
 
-void allocate_ycc_pixels_memory(YCC_image_t **ycc_img)
+void allocate_ycc_pixels_memory(YCC_image_t *ycc_img)
 {
     ycc_img->pixel_count = 0;
     ycc_img->pixels = (YCC_pixel_t *)malloc(sizeof(YCC_pixel_t) * ycc_img->height * ycc_img->width);
