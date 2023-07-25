07a2e266303459b0af05109b963bab46dcc54d49
Adil <adilzafar66@gmail.com>
2023-07-24 22:06:22 -0700

    oo

--- unoptimized/colorspace_converter2.c
+++ unoptimized/colorspace_converter2.c
@@ -66,14 +66,14 @@ int main(int argc, char *argv[])
 
     // Compute and allocate conversions
     compute_ycc_from_rgb(rgb_in_img, ycc_out_img);
-    // downsample_ycc_chroma(ycc_out_img);
+    downsample_ycc_chroma(ycc_out_img);
     compute_rgb_from_ycc(ycc_out_img, rgb_out_img);
 
-    // Write the images to output files
-    ycc_pixels_file_handler(ycc_out_img, out_file_luma, write_luma);
-    ycc_pixels_file_handler(ycc_out_img, out_file_cb, write_cb);
-    ycc_pixels_file_handler(ycc_out_img, out_file_cr, write_cr);
-    rgb_pixels_file_handler(rgb_out_img, out_file_rgb, write_rgb);
+    // // Write the images to output files
+    // ycc_pixels_file_handler(ycc_out_img, out_file_luma, write_luma);
+    // ycc_pixels_file_handler(ycc_out_img, out_file_cb, write_cb);
+    // ycc_pixels_file_handler(ycc_out_img, out_file_cr, write_cr);
+    // rgb_pixels_file_handler(rgb_out_img, out_file_rgb, write_rgb);
 
     // Free memory
     free(rgb_in_img->pixels);
