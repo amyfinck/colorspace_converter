9402f4fa871ddf7432dbe8080b41b9fdc38f7237
Adil Zafar <adil.khan@primeeng.ca>
2023-07-25 13:33:32 -0700

    oo

--- unoptimized/transformers.h
+++ unoptimized/transformers.h
@@ -28,26 +28,20 @@ uint8_t compute_ycc_cr(uint8_t R, uint8_t G, uint8_t B)
 
 uint8_t compute_rgb_r(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int64_t temp = (74 * (Y - 16)) + (102 * (Cr - 128));
-    if (temp < 0)
-        temp = 0;
-    return (uint8_t)((temp + (1 << (K - 1))) >> K);
+    int64_t red = FP_ROUND(Y + 1.402 * (Cr - 128));
+    return (uint8_t)(red < 0 ? 0 : (red > 255 ? 255 : red));
 }
 
 uint8_t compute_rgb_g(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int64_t temp = (74 * (Y - 16)) - (52 * (Cr - 128)) - (25 * (Cb - 128));
-    if (temp < 0)
-        temp = 0;
-    return (uint8_t)((temp + (1 << (K - 1))) >> K);
+    int64_t green = FP_ROUND(Y - 0.344136 * (Cb - 128) - 0.714136 * (Cr - 128));
+    return (uint8_t)(green < 0 ? 0 : (green > 255 ? 255 : green));
 }
 
 uint8_t compute_rgb_b(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int64_t temp = (74 * (Y - 16)) + (129 * (Cb - 128));
-    if (temp < 0)
-        temp = 0;
-    return (uint8_t)((temp + (1 << (K - 1))) >> K);
+    int64_t blue = FP_ROUND(Y + 1.772 * (Cb - 128));
+    return (uint8_t)(blue < 0 ? 0 : (blue > 255 ? 255 : blue));
 }
 
 #endif
