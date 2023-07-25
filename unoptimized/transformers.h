b18d95dc107a817f5aacd3460937706595d7ed96
Adil Zafar <adil.khan@primeeng.ca>
2023-07-25 13:51:45 -0700

    oo

--- unoptimized/transformers.h
+++ unoptimized/transformers.h
@@ -26,8 +26,9 @@ uint8_t compute_ycc_cr(uint8_t R, uint8_t G, uint8_t B)
 
 uint8_t compute_rgb_r(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int16_t y = (int16_t)Y[i] - 16;
-    int16_t cr = (int16_t)Cr[i] - 128;
+    int16_t y = (int16_t)Y - 16;
+    int16_t cb = (int16_t)Cb - 128;
+    int16_t cr = (int16_t)Cr - 128;
 
     int16_t red = (298 * y + 409 * cr + 128) >> 8;
     return (uint8_t)(red < 0 ? 0 : (red > 255 ? 255 : red));
@@ -35,9 +36,9 @@ uint8_t compute_rgb_r(uint8_t Y, uint8_t Cb, uint8_t Cr)
 
 uint8_t compute_rgb_g(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int16_t y = (int16_t)Y[i] - 16;
-    int16_t cb = (int16_t)Cb[i] - 128;
-    int16_t cr = (int16_t)Cr[i] - 128;
+    int16_t y = (int16_t)Y - 16;
+    int16_t cb = (int16_t)Cb - 128;
+    int16_t cr = (int16_t)Cr - 128;
 
     int16_t green = (298 * y - 100 * cb - 208 * cr + 128) >> 8;
     return (uint8_t)(green < 0 ? 0 : (green > 255 ? 255 : green));
@@ -45,9 +46,9 @@ uint8_t compute_rgb_g(uint8_t Y, uint8_t Cb, uint8_t Cr)
 
 uint8_t compute_rgb_b(uint8_t Y, uint8_t Cb, uint8_t Cr)
 {
-    int16_t y = (int16_t)Y[i] - 16;
-    int16_t cb = (int16_t)Cb[i] - 128;
-    int16_t cr = (int16_t)Cr[i] - 128;
+    int16_t y = (int16_t)Y - 16;
+    int16_t cb = (int16_t)Cb - 128;
+    int16_t cr = (int16_t)Cr - 128;
 
     int16_t blue = (298 * y + 516 * cb + 128) >> 8;
     return (uint8_t)(blue < 0 ? 0 : (blue > 255 ? 255 : blue));
