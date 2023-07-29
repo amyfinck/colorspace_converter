beea30fdc229dc780aba5f4ad352ea71a8370926
Adil <adilzafar66@gmail.com>
2023-07-29 16:18:27 -0700

    oo

--- src/colorspace_converter.h
+++ src/colorspace_converter.h
@@ -16,11 +16,6 @@
 void get_image_info(RGB_image_t *input_rgb_img, FILE* file);
 void read_pixels(RGB_image_t *input_rgb_img, FILE* file);
 void write_header(RGB_image_t *input_rgb_img, FILE* file_to_write, FILE* reference_file);
-void resize_file(FILE* file, uint32_t width, uint32_t height);
-void get_luma(RGB_image_t *input_rgb_img, YCC_image_t *output_ycc_img);
-void get_chroma(RGB_image_t *input_rgb_img, YCC_image_t *output_ycc_img);
-void downsample_chroma(RGB_image_t *input_rgb_img, YCC_image_t *output_ycc_img);
-void ycc_to_rgb(RGB_image_t *input_rgb_img, RGB_image_t *output_rgb_img, YCC_image_t *output_ycc_img);
 void write_rgb(RGB_image_t *input_rgb_img, RGB_image_t *output_rgb_img, FILE* rgb_file);
 void write_ycc_components(RGB_image_t *input_rgb_img, YCC_image_t *output_ycc_img, FILE* luma_fp, FILE* cb_fp, FILE* cr_fp);
 
