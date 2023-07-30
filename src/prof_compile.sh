b5e4449fb05fac5276306178c0406b06c468ab54
amyfinck <amyfinck@outlook.com>
2023-07-29 22:20:24 -0700

    update profile script + add profile

--- src/prof_compile.sh
+++ src/prof_compile.sh
@@ -6,10 +6,16 @@ echo Copying files to /tmp/tmp_dir/
 mkdir -p /tmp/tmp_dir/
 cp ./colorspace_converter.c /tmp/tmp_dir/
 cp ./colorspace_converter.h /tmp/tmp_dir/
+cp ./allocators.h /tmp/tmp_dir/
+cp ./bmp_parser.h /tmp/tmp_dir/
+cp ./constants.h /tmp/tmp_dir/
+cp ./converters.h /tmp/tmp_dir/
+cp ./transformers.h /tmp/tmp_dir/
+cp ./utilities.h /tmp/tmp_dir/
 # add more files as they are added to our project
 
 echo Compiling unoptimized colorspace converter
-/opt/arm/4.3.2/bin/arm-linux-gcc -pg -static -mfloat-abi=softfp -mfpu=neon -O3 /tmp/tmp_dir/colorspace_converter.c -o cc.exe
+/opt/arm/4.3.2/bin/arm-linux-gcc -pg -static -mfpu=neon -O3 /tmp/tmp_dir/colorspace_converter.c -o cc.exe
 
 wait $process_id
 
