4a11a876d2f2eda1620e38224a698138c3c66046
Adil <adilzafar66@gmail.com>
2023-07-30 22:39:00 -0700

    oo

--- src/bmp_parser.h
+++ src/bmp_parser.h
@@ -7,7 +7,6 @@
 
 #define PIXEL_M 3
 
-
 uint32_t get_row_byte_count(uint32_t img_width)
 {
     uint32_t row_bytes = img_width * PIXEL_M;
@@ -24,43 +23,6 @@ uint32_t get_buffer_row_bytes(uint32_t img_width)
     uint32_t row_byte_count = get_row_byte_count(img_width);
     uint32_t row_pixels_byte_count = img_width * PIXEL_M;
     return row_byte_count - row_pixels_byte_count;
-}
-
-void read_rgb(RGB_image_t *in_img, FILE *in_file, uint32_t index)
-{
-    fread(&in_img->pixels[index].B, 1, 1, in_file);
-    fread(&in_img->pixels[index].G, 1, 1, in_file);
-    fread(&in_img->pixels[index].R, 1, 1, in_file);
-}
-
-void write_rgb(RGB_image_t *in_img, FILE *out_file, uint32_t index)
-{
-    fwrite(&in_img->pixels[index].B, 1, 1, out_file);
-    fwrite(&in_img->pixels[index].G, 1, 1, out_file);
-    fwrite(&in_img->pixels[index].R, 1, 1, out_file);
-}
-
-void write_luma(YCC_image_t *in_img, FILE *out_file, uint32_t index)
-{
-    fwrite(&in_img->pixels[index].Y, 1, 1, out_file);
-    fwrite(&in_img->pixels[index].Y, 1, 1, out_file);
-    fwrite(&in_img->pixels[index].Y, 1, 1, out_file);
-}
-
-void write_cb(YCC_image_t *in_img, FILE *out_file, uint32_t index)
-{
-    uint8_t base_color = 128;
-    fwrite(&in_img->pixels[index].Cb, 1, 1, out_file);
-    fwrite(&base_color, 1, 1, out_file);
-    fwrite(&base_color, 1, 1, out_file);
-}
-
-void write_cr(YCC_image_t *in_img, FILE *out_file, uint32_t index)
-{
-    uint8_t base_color = 128;
-    fwrite(&base_color, 1, 1, out_file);
-    fwrite(&base_color, 1, 1, out_file);
-    fwrite(&in_img->pixels[index].Cr, 1, 1, out_file);
 }
 
 void read_pixels_rgb(header_t *header, RGB_image_t *img, FILE *file)
@@ -158,24 +120,6 @@ void write_pixels_cr(header_t *header, YCC_image_t *img, FILE *file)
             fwrite(&base_color, 1, 1, file);
             fwrite(&base_color, 1, 1, file);
             fwrite(&img->pixels[index].Cr, 1, 1, file);
-            if (column == header->width - 1 && buffer_row_bytes != 0)
-                fseek(file, buffer_row_bytes, SEEK_CUR);
-            ++header->pixel_count;
-        }
-    }
-}
-
-void ycc_pixels_file_handler(header_t *header, YCC_image_t *img, FILE *file, void (*io_func)(YCC_image_t *, FILE *, uint32_t))
-{
-    exit_on_error(fseek(file, header->offset, SEEK_SET) != 0, "Error: Seeking pixel start position failed");
-    uint32_t buffer_row_bytes = get_buffer_row_bytes(header->width);
-    uint32_t row, column;
-    for (row = 0; row < header->height; row++)
-    {
-        for (column = 0; column < header->width; column++)
-        {
-            uint32_t index = row * header->width + column;
-            io_func(img, file, index);
             if (column == header->width - 1 && buffer_row_bytes != 0)
                 fseek(file, buffer_row_bytes, SEEK_CUR);
             ++header->pixel_count;
