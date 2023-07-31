bc561aab9c2492a6cc99b9bf9d6d60c314ad4e7a
Adil <adilzafar66@gmail.com>
2023-07-30 18:07:15 -0700

    kk

--- src/bmp_parser.h
+++ src/bmp_parser.h
@@ -54,30 +54,15 @@ void get_image_info(header_t *header, FILE* file)
 {
     // get file size
     header->file_size = get_file_size(file);
-
-    // get offset, width, and height
-    if (fseek(file, 10, SEEK_SET) != 0) {
-        printf("Error seeking to offset position\n"); exit(1);
-    }
-    fread(&header->offset, 4, 1, file);
-    if (fseek(file, 18, SEEK_SET) != 0){
-        printf("Error seeking to width position\n"); exit(1);
-    }
-    fread(&header->width, 4, 1, file);
-    if (fseek(file, 22, SEEK_SET) != 0) {
-        printf("Error seeking to offset position\n"); exit(1);
-    }
-    fread(&header->height, 4, 1, file);
+    get_file_offset(header, file);
+    get_file_width(header, file);
+    get_file_height(header, file);
 
     uint32_t bytes_per_row = header->width * 3; // 3 bytes per pixel
     if (bytes_per_row % 4 != 0)
-    {
         header->padding= 4 - (bytes_per_row % 4);
-    }
     else
-    {
         header->padding = 0;
-    }
 }
 
 void read_rgb(RGB_image_t *in_img, FILE *in_file, uint32_t index)
