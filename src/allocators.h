5e97a2b7d56a2843d2996cf138a4b9649696f9a6
Adil <adilzafar66@gmail.com>
2023-07-30 18:57:23 -0700

    oo

--- src/allocators.h
+++ src/allocators.h
@@ -21,10 +21,10 @@ void allocate_ycc_memory(YCC_image_t **ycc_img)
     exit_on_error(*ycc_img == NULL, "Malloc for yyc image failed");
 }
 
-void allocate_rgb_pixels_memory(uint32_t height, uint32_t width, RGB_image_t **rgb_img)
+void allocate_rgb_pixels_memory(uint32_t height, uint32_t width, RGB_image_t *rgb_img)
 {
-    (*rgb_img)->pixels = (RGB_pixel_t *)malloc(sizeof(RGB_pixel_t) * height * width);
-    exit_on_error((*rgb_img)->pixels == NULL, "Malloc for RGB pixels failed");
+    rgb_img->pixels = (RGB_pixel_t *)malloc(sizeof(RGB_pixel_t) * height * width);
+    exit_on_error(rgb_img->pixels == NULL, "Malloc for RGB pixels failed");
 }
 
 void allocate_ycc_pixels_memory(uint32_t height, uint32_t width, YCC_image_t *ycc_img)
--- src/colorspace_converter.c
+++ src/colorspace_converter.c
@@ -76,7 +76,7 @@ int main(int argc, char* argv[] )
     
     // create image structs
     allocate_rgb_memory(&input_rgb_img);
-    allocate_rgb_pixels_memory(header->height, header->width, &input_rgb_img);
+    allocate_rgb_pixels_memory(header->height, header->width, input_rgb_img);
     allocate_rgb_memory(&output_rgb_img);
     allocate_ycc_memory(&output_ycc_img);
     allocate_header_memory(&header);
@@ -99,7 +99,7 @@ int main(int argc, char* argv[] )
 
     // Allocate pixel memory
     allocate_ycc_pixels_memory(header->height, header->width, output_ycc_img);
-    allocate_rgb_pixels_memory(header->height, header->width, &output_rgb_img);
+    allocate_rgb_pixels_memory(header->height, header->width, output_rgb_img);
 
     // Calculate YCC values for OutputImage
     get_luma(header->pixel_count, input_rgb_img, output_ycc_img);
