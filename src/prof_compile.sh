ffc75247a791c4075e21895aa16ace6b6b944f7c
Amy Finck <54650318+amyfinck@users.noreply.github.com>
2023-07-31 13:44:12 -0700

    no more function definitions in headers

--- src/prof_compile.sh
+++ src/prof_compile.sh
@@ -6,16 +6,22 @@ echo Copying files to /tmp/tmp_dir/
 mkdir -p /tmp/tmp_dir/
 cp ./colorspace_converter.c /tmp/tmp_dir/
 cp ./colorspace_converter.h /tmp/tmp_dir/
+
 cp ./bmp_parser.h /tmp/tmp_dir/
 cp ./bmp_parser.c /tmp/tmp_dir/
+
 cp ./constants.h /tmp/tmp_dir/
+
 cp ./converters.h /tmp/tmp_dir/
+cp ./converters.c /tmp/tmp_dir/
+
 cp ./transformers.h /tmp/tmp_dir/
+cp ./transformers.c /tmp/tmp_dir/
+
 cp ./utilities.h /tmp/tmp_dir/
-# add more files as they are added to our project
 
 echo Compiling unoptimized colorspace converter
-/opt/arm/4.3.2/bin/arm-linux-gcc -pg -static -mfpu=neon -O3 /tmp/tmp_dir/colorspace_converter.c /tmp/tmp_dir/bmp_parser.c -o cc.exe
+/opt/arm/4.3.2/bin/arm-linux-gcc -pg -static -mfpu=neon -O3 /tmp/tmp_dir/colorspace_converter.c /tmp/tmp_dir/bmp_parser.c /tmp/tmp_dir/converters.c /tmp/tmp_dir/transformers.c cc.exe
 
 wait $process_id
 
