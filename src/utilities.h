4a82725f158df5fb212ec3b89358af9a7269115a
Adil <adilzafar66@gmail.com>
2023-07-30 21:57:56 -0700

    oo

--- src/utilities.h
+++ src/utilities.h
@@ -34,4 +34,17 @@ void check_height_width(uint32_t width, uint32_t height)
     exit_on_error(condition, "width and height must be divisible by 2");
 }
 
+void write_header(uint32_t offset, FILE* file_to_write, FILE* reference_file)
+{
+    char buffer[offset];
+    size_t bytes_read;
+    size_t total_bytes_read = 0;
+    fseek(reference_file, 0, SEEK_SET);
+    while (total_bytes_read < offset && (bytes_read = fread(buffer, 1, offset - total_bytes_read, reference_file)) > 0)
+    {
+        exit_on_error(fwrite(buffer, 1, bytes_read, file_to_write) == -1, "Writing to luma file failed");
+        total_bytes_read += bytes_read;
+    }
+}
+
 #endif
