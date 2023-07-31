6cae4a51f577e5abd8b8f50fe7ed6db3484d3d56
Adil <adilzafar66@gmail.com>
2023-07-30 22:14:30 -0700

    fix

--- src/bmp_parser.h
+++ src/bmp_parser.h
@@ -73,9 +73,9 @@ void read_pixels_rgb(header_t *header, RGB_image_t *img, FILE *file)
         for (column = 0; column < header->width; column++)
         {
             uint32_t index = row * header->width + column;
-            fread(&img->pixels[index].B, 1, 1, in_file);
-            fread(&img->pixels[index].G, 1, 1, in_file);
-            fread(&img->pixels[index].R, 1, 1, in_file);
+            fread(&img->pixels[index].B, 1, 1, file);
+            fread(&img->pixels[index].G, 1, 1, file);
+            fread(&img->pixels[index].R, 1, 1, file);
             if (column == header->width - 1 && buffer_row_bytes != 0)
                 fseek(file, buffer_row_bytes, SEEK_CUR);
             ++header->pixel_count;
