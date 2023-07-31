80a653a05bd28a6a1423df2f3b8c419782fd23c9
Adil <adilzafar66@gmail.com>
2023-07-30 22:15:05 -0700

    fix

--- src/utilities.h
+++ src/utilities.h
@@ -5,17 +5,6 @@
 
 #define PIXEL_M 3
 
-uint32_t get_row_byte_count(uint32_t img_width)
-{
-    uint32_t row_bytes = img_width * PIXEL_M;
-    if (row_bytes % 4 != 0)
-    {
-        uint32_t padding = 4 - (row_bytes % 4);
-        row_bytes += padding;
-    }
-    return row_bytes;
-}
-
 void resize_file(FILE* file, uint32_t width, uint32_t height)
 {
     if (fseek(file, 18, SEEK_SET) != 0){
