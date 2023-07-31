b0de75a86b40531b58b7aeecec8e0ab7295de266
Adil <adilzafar66@gmail.com>
2023-07-30 17:36:46 -0700

    remove declarations

--- src/colorspace_converter.h
+++ src/colorspace_converter.h
@@ -13,11 +13,7 @@
 #include "bmp_parser.h"
 
 void get_image_info(header_t *header, FILE* file);
-void resize_file(FILE* file, uint32_t width, uint32_t height);
-void read_pixels(header_t *header, RGB_image_t *input_rgb_img, FILE* file);
-void write_rgb_file(header_t *header, RGB_image_t *output_rgb_img, FILE* rgb_file);
 void write_header(uint32_t offset, FILE* file_to_write, FILE* reference_file);
-void write_ycc_components(header_t *header, YCC_image_t *output_ycc_img, FILE* luma_fp, FILE* cb_fp, FILE* cr_fp);
 
 
 #endif //COLORSPACE_CONVERTER_COLORSPACE_CONVERTER_H
