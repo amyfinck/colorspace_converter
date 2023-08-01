2c90e0480578a7f0eab16d65c26830817a3f045e
Adil <adilzafar66@gmail.com>
2023-07-31 20:35:39 -0700

    optimize loop

--- src/converters.c
+++ src/converters.c
@@ -3,7 +3,7 @@
 void rgb_to_ycc(uint32_t pixel_count, RGB_image_t *input_rgb_img, YCC_image_t *output_ycc_img)
 {
     uint32_t i;
-    for(i = pixel_count - 1; i != 0; i--)
+    for(i = pixel_count - 1; i != 0; --i)
     {
         uint8_t R = input_rgb_img->pixels[i].R;
         uint8_t G = input_rgb_img->pixels[i].G;
@@ -29,7 +29,7 @@ void rgb_to_ycc(uint32_t pixel_count, RGB_image_t *input_rgb_img, YCC_image_t *o
 void ycc_to_rgb(uint32_t pixel_count, RGB_image_t *output_rgb_img, YCC_image_t *output_ycc_img)
 {
     uint32_t i;
-    for(i = 0; i < pixel_count; i++)
+    for(i = pixel_count - 1; i != 0; --i)
     {
         uint8_t Y = output_ycc_img->pixels[i].Y;
         uint8_t Cb = output_ycc_img->pixels[i].Cb;
@@ -44,11 +44,10 @@ void ycc_to_rgb(uint32_t pixel_count, RGB_image_t *output_rgb_img, YCC_image_t *
 void downsample_chroma(uint32_t height, uint32_t width, YCC_image_t *output_ycc_img)
 {
     // TODO this should probably be a scaled down image
-    uint32_t y;
-    for(y = 0; y < height; y+=2)
+    uint32_t y, x;
+    for(y = height - 1; y != 0; y-=2)
     {
-        uint32_t x;
-        for(x = 0; x < width; x+=2)
+        for(x = width - 1; x != 0; x-=2)
         {
             uint32_t bottom_left = y * width + x;
             uint32_t bottom_right = bottom_left + 1;
@@ -56,9 +55,9 @@ void downsample_chroma(uint32_t height, uint32_t width, YCC_image_t *output_ycc_
             uint32_t top_right = top_left + 1;
 
             int32_t Cb_total = (int32_t)output_ycc_img->pixels[bottom_left].Cb + (int32_t)output_ycc_img->pixels[bottom_right].Cb + (int32_t)output_ycc_img->pixels[top_left].Cb + (int32_t)output_ycc_img->pixels[top_right].Cb;
-            int8_t Cb_avg = (int8_t)(Cb_total / 4);
+            int8_t Cb_avg = (int8_t)((Cb_total >> 2) & 0xFF);
             int32_t Cr_total = (int32_t)output_ycc_img->pixels[bottom_left].Cr + (int32_t)output_ycc_img->pixels[bottom_right].Cr + (int32_t)output_ycc_img->pixels[top_left].Cr + (int32_t)output_ycc_img->pixels[top_right].Cr;
-            int8_t Cr_avg = (int8_t)(Cr_total / 4);
+            int8_t Cr_avg = (int8_t)((Cr_total >> 2) & 0xFF);
 
             output_ycc_img->pixels[bottom_left].Cb = Cb_avg;
             output_ycc_img->pixels[bottom_right].Cb = Cb_avg;
