1e9431bb19eb65409dc3220b024905cb96bf75e9
Adil <adilzafar66@gmail.com>
2023-07-31 20:07:23 -0700

    oo

--- src/compile.sh
+++ src/compile.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 process_id=$!
 
-echo Copying files to /tmp/tmp_dir/
+echo Copying files to /tmp/tmp_comp/
 
-mkdir -p /tmp/tmp_dir/
-cp ./colorspace_converter.c /tmp/tmp_dir/
-cp ./colorspace_converter.h /tmp/tmp_dir/
+mkdir -p /tmp/tmp_comp/
+cp ./colorspace_converter.c /tmp/tmp_comp/
+cp ./colorspace_converter.h /tmp/tmp_comp/
 
-cp ./bmp_parser.h /tmp/tmp_dir/
-cp ./bmp_parser.c /tmp/tmp_dir/
+cp ./bmp_parser.h /tmp/tmp_comp/
+cp ./bmp_parser.c /tmp/tmp_comp/
 
-cp ./constants.h /tmp/tmp_dir/
+cp ./constants.h /tmp/tmp_comp/
 
-cp ./converters.h /tmp/tmp_dir/
-cp ./converters.c /tmp/tmp_dir/
+cp ./converters.h /tmp/tmp_comp/
+cp ./converters.c /tmp/tmp_comp/
 
-cp ./transformers.h /tmp/tmp_dir/
-cp ./transformers.c /tmp/tmp_dir/
+cp ./transformers.h /tmp/tmp_comp/
+cp ./transformers.c /tmp/tmp_comp/
 
-cp ./utilities.h /tmp/tmp_dir/
+cp ./utilities.h /tmp/tmp_comp/
 # add more files as they are added to our project
 
 echo Compiling colorspace converter
-/opt/arm/4.3.2/bin/arm-linux-gcc -static -mfpu=neon -O3 -I/tmp/tmp_dir /tmp/tmp_dir/colorspace_converter.c /tmp/tmp_dir/bmp_parser.c /tmp/tmp_dir/converters.c /tmp/tmp_dir/transformers.c /tmp/tmp_dir/utilities.c -o cc.exe
+/opt/arm/4.3.2/bin/arm-linux-gcc -static -mfpu=neon -O3 -I/tmp/tmp_comp /tmp/tmp_comp/colorspace_converter.c /tmp/tmp_comp/bmp_parser.c /tmp/tmp_comp/converters.c /tmp/tmp_comp/transformers.c /tmp/tmp_comp/utilities.c -o cc.exe
 
 wait $process_id
 
