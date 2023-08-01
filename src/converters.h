00ba23b565824a3d508503bc13abf2cfcb20d977
Adil <adilzafar66@gmail.com>
2023-07-31 21:57:44 -0700

    oo

--- src/converters.h
+++ src/converters.h
@@ -5,7 +5,7 @@
 #include "transformers.h"
 
 void ycc_to_rgb(uint32_t pixel_count, RGB_image_t *output_rgb_img, YCC_image_t *output_ycc_img);
-void rgb_to_ycc(uint32_t pixel_count, RGB_image_t *input_rgb_img, YCC_image_t *output_ycc_img);
+void rgb_to_ycc(header_t* header, YCC_pixel_t *output_ycc_img, FILE* in_file);
 void downsample_chroma(uint32_t height, uint32_t width, YCC_image_t *output_ycc_img);
 
 #endif
