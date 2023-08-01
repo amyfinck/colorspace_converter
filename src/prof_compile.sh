71a25b9777d5eec2497f35299b0ae81fc2069606
Adil <adilzafar66@gmail.com>
2023-07-31 22:40:50 -0700

    oo

--- src/prof_compile.sh
+++ src/prof_compile.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 process_id=$!
 
-echo Copying files to /tmp/tmp_dir/
+echo Copying files to /tmp/tmp_comp/
 
-mkdir -p /tmp/tmp_dir/
-cp ./colorspace_converter.c /tmp/tmp_dir/
-cp ./colorspace_converter.h /tmp/tmp_dir/
+mkdir -p /tmp/tmp_comp/
+cp ./colorspace_converter.c /tmp/tmp_comp/
+cp ./colorspace_converter.h /tmp/tmp_comp/
 # add more files as they are added to our project
 
 echo Compiling unoptimized colorspace converter
-/opt/arm/4.3.2/bin/arm-linux-gcc -pg -static -mfloat-abi=softfp -mfpu=neon -O3 /tmp/tmp_dir/colorspace_converter.c -o cc.exe
+/opt/arm/4.3.2/bin/arm-linux-gcc -pg -static -mfloat-abi=softfp -mfpu=neon -O3 /tmp/tmp_comp/colorspace_converter.c -o cc.exe
 
 wait $process_id
 
